8cdb0efec2da2a0295bdf37f422b11584ee56433
Brent Vatne <brentvatne@gmail.com>
2015-10-08 15:57:51 -0700

    onLoadMore becomes onLoadMoreAsync, automatically catch errors

--- InfiniteScrollView.js
+++ InfiniteScrollView.js
@@ -5,30 +5,42 @@ let ScrollableMixin = require('react-native-scrollable-mixin');
 let {
   PropTypes,
   ScrollView,
+  View,
 } = React;
 
 let cloneReferencedElement = require('react-native-clone-referenced-element');
 
 let DefaultLoadingIndicator = require('./DefaultLoadingIndicator');
 
-class InfiniteScrollView {
+class InfiniteScrollView extends React.Component {
   static propTypes = {
     ...ScrollView.propTypes,
     distanceToLoadMore: PropTypes.number.isRequired,
     canLoadMore: PropTypes.bool.isRequired,
     isLoadingMore: PropTypes.bool.isRequired,
-    onLoadMore: PropTypes.func.isRequired,
+    onLoadError: PropTypes.func,
+    onLoadMoreAsync: PropTypes.func.isRequired,
     renderLoadingIndicator: PropTypes.func.isRequired,
+    renderLoadingErrorIndicator: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
-    distanceToLoadMore: 1000,
+    distanceToLoadMore: 1500,
     canLoadMore: false,
     isLoadingMore: false,
-    scrollEventThrottle: 100,
+    scrollEventThrottle: 16,
     renderLoadingIndicator: () => <DefaultLoadingIndicator />,
+    renderLoadingErrorIndicator: () => <View />,
     renderScrollComponent: props => <ScrollView {...props} />,
   };
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      isDisplayingError: false,
+    };
+  }
 
   getScrollResponder(): ReactComponent {
     return this._scrollComponent.getScrollResponder();
@@ -39,8 +51,15 @@ class InfiniteScrollView {
   }
 
   render() {
-    if (this.props.isLoadingMore) {
-      var loadingIndicator = React.cloneElement(
+    let statusIndicator;
+
+    if (this.state.isDisplayingError) {
+      statusIndicator = React.cloneElement(
+        this.props.renderLoadingErrorIndicator(this._retryOnLoadMore.bind(this)),
+        { key: 'loading-error-indicator' },
+      );
+    } else if (this.props.canLoadMore) {
+      statusIndicator = React.cloneElement(
         this.props.renderLoadingIndicator(),
         { key: 'loading-indicator' },
       );
@@ -52,7 +71,7 @@ class InfiniteScrollView {
     } = this.props;
     Object.assign(props, {
       onScroll: this._handleScroll.bind(this),
-      children: [this.props.children, loadingIndicator],
+      children: [this.props.children, statusIndicator],
     });
 
     return cloneReferencedElement(renderScrollComponent(props), {
@@ -70,8 +89,20 @@ class InfiniteScrollView {
     }
 
     if (this._distanceFromEnd(event) < this.props.distanceToLoadMore) {
-      this.props.onLoadMore();
+      this._onLoadMore();
     }
+  }
+
+  _onLoadMore() {
+    this.props.onLoadMoreAsync().catch((error) => {
+      this.props.onLoadError && this.props.onLoadError(error);
+      this.setState({isDisplayingError: true});
+    });
+  }
+
+  _retryOnLoadMore() {
+    this.setState({isDisplayingError: false});
+    this._onLoadMore();
   }
 
   _distanceFromEnd(event): number {
